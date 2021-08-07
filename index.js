650cd2826512b0bf79f9b6062366fab81078e71c
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-08-07 18:12:18 -0400

    removed user story and acceptance criteria as inputs and instead passed it as static

--- index.js
+++ index.js
@@ -13,16 +13,17 @@ inquirer.prompt([{
             message: 'What is this homework about?',
             name: 'about'
         },
-        {
-            type: 'input',
-            message: 'User Story',
-            name: 'userStory'
-        },
-        {
-            type: 'input',
-            message: 'Acceptance Criteria',
-            name: 'acceptanceCriteria'
-        },  
+        // User Story and Acceptance Criteria can be taken as input from the user however this is being hardcoded for the sake of this homework exercise
+        // {
+        //     type: 'input',
+        //     message: 'User Story',
+        //     name: 'userStory'
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Acceptance Criteria',
+        //     name: 'acceptanceCriteria'
+        // },  
         {
             type: 'input',
             message: 'Github Repository URL?',
@@ -51,12 +52,23 @@ inquirer.prompt([{
             fs.appendFile('README.md', 
 // few sentences about the HW
             (`${response.about}\n\n` +
+
 // User Story
-`# User Story:\n
-    ${response.userStory}\n\n` +
+// `# User Story:\n ${response.userStory}\n\n` + --> If user story was provided as a part of input it would be formatted using this line instead of the next hardcoded version
+`# User Story:\nAS A developer\nI WANT a README generator\nSO THAT I can quickly create a professional README for a new project\n\n`+
+
 // Acceptance Criteria
+// `# Acceptance Criteria:\n ${response.acceptanceCriteria}\n\n` + --> If acceptance criteria was provided as a part of input it would be formatted using this line instead of the next hardcoded version
 `# Acceptance Criteria:\n
-    ${response.acceptanceCriteria}\n\n` +
+GIVEN a command-line application that accepts user input
+✓ WHEN I am prompted for information about my application repository THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+✓ WHEN I enter my project title THEN this is displayed as the title of the README
+✓ WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+✓ WHEN I choose a license for my application from a list of options THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+✓ WHEN I enter my GitHub username THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+✓ WHEN I enter my email address THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+✓ WHEN I click on the links in the Table of Contents THEN I am taken to the corresponding section of the README\n\n` +
+
 // DNB's Solutions with links to repo and github page
         `# DNB's SOLUTION:\n` +
          `#### UNC Boot Camp Submission: \n` +
