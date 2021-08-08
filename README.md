88b383188b0f04dcb3c8989c11c1cef065ef360f
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-08-08 17:35:52 -0400

    almost done

--- README.md
+++ README.md
@@ -1,30 +1,30 @@
 # 09--Professional_Dynamically_Generated_ReadME_using_Node.Js
 
 ## Table of Contents
-👉 [Qns -Contact Information](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js#for-any-questions-reach-me-via-my-username-at-dionnenoellabarretto-or-email-me-at--denverdionnegmailcom)
+👉 [Qns -Contact Information](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js#for-any-questions-reach-me-via-my-username-at-dionnenoellabarretto-or-email-me-at--denverdionnegmailcom)
 
-👉 [License](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js#license-badges)
+👉 [License](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js#license-badges)
 
-👉 [Description](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js#description)
+👉 [Description](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js#description)
 
-👉 [Instructions](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js#installation-instructions)
+👉 [Instructions](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js#installation-instructions)
 
-👉 [User Story (Usage)](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js#user-story-usage)
+👉 [User Story (Usage)](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js#user-story-usage)
 
-👉 [Acceptance Criteria](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js#acceptance-criteria)
+👉 [Acceptance Criteria](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js#acceptance-criteria)
 
-👉 [Contributors](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js#contributors)
+👉 [Contributors](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js#contributors)
 
-👉 [DNB Solution](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js#dnbs-solution)
+👉 [DNB Solution](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js#dnbs-solution)
 
 
- #### For any questions, reach me via my ![Github Logo](./assets/images/octocat.png?raw=true "Github Logo")username at: [dionnenoellabarretto ](https://github.com/dionnenoellabarretto ) or email me at: 📧denverdionne@gmail.com.
+ #### For any questions, reach me via my ![Github Logo](./assets/images/octocat.png?raw=true "Github Logo")username at: [dionnenoellabarretto](https://github.com/dionnenoellabarretto) or email me at: 📧denverdionne@gmail.com.
 
 ## License:
  This repository is released under the license of: [MIT](https://opensource.org/licenses/MIT)
 
 ## Description: 
-In this homework, our task is 
+In this homework exercise, our task is to create a command-line application that dynamically generates a professional README.md file from a user's input using the [Inquirer package](https://www.npmjs.com/package/inquirer).
 
 ## Installation Instructions: 
 To generate your own README, first run 'npm install' or 'npm i' in order to install the npm package dependencies as specified in the package.json. Then run the application using 'node index.js'.
@@ -34,7 +34,7 @@ Not Applicable for this exercise
 
 ## DNB's SOLUTION:
 #### UNC Boot Camp Submission: 
-🗂️ [Github Repository](https://github.com/dionnenoellabarretto /09--Professional_ReadME_Node.Js) | 📄 [Github Page](https://dionnenoellabarretto .github.io/09--Professional_ReadME_Node.Js)
+🗂️ [Github Repository](https://github.com/dionnenoellabarretto/09--Professional_ReadME_Node.Js) | 📄 [Github Page](https://dionnenoellabarretto.github.io/09--Professional_ReadME_Node.Js)
 
 ## User Story (Usage):
  AS A developer I WANT a README generator SO THAT I can quickly create a professional README for a new project.
--- index.js
+++ index.js
@@ -44,7 +44,7 @@ inquirer.prompt([{
             message: 'Github Repository Name?',
             name: 'repoName'
         },
-       //If User Story and Acceptance Criteria were hardcoded these following 2 could be skipped
+        //If User Story and Acceptance Criteria were hardcoded these following 2 could be skipped
         {
             type: 'input',
             message: 'User Story:',
@@ -54,25 +54,27 @@ inquirer.prompt([{
             type: 'input',
             message: 'Acceptance Criteria:',
             name: 'acceptanceCriteria'
-        },  
+        },
     ])
 
     // Writing to a markdown file
     .then((response) => {
         title();
+
         function title() {
-// writes to the README.md file and appends a title to the first line in the file with the cursor moved to the second new line
+            // writes to the README.md file and appends a title to the first line in the file with the cursor moved to the second new line
             fs.writeFile('README.md', (`# ${response.title}\n\n`),
-// ternary operator for error message being requested as a console log in case of errors lest a console log message denoting success
+                // ternary operator for error message being requested as a console log in case of errors lest a console log message denoting success
                 (err) => err ? console.error(err) : console.log('A README.md file with user supplied content and a table of contents section was successfully created!'))
         }
 
-//table of contents
+        //table of contents
         toc();
+
         function toc() {
             fs.appendFile('README.md',
-// dynamically generating a placeholder based on the users github username and repo section as outlined https://www.setcorrect.com/portfolio/work11/  
-            (`## Table of Contents\n` +
+                // dynamically generating a placeholder based on the users github username and repo section as outlined https://www.setcorrect.com/portfolio/work11/  
+                (`## Table of Contents\n` +
                     `👉 [Qns -Contact Information](https://github.com/${response.username}/${response.repoName}#for-any-questions-reach-me-via-my-username-at-dionnenoellabarretto-or-email-me-at--denverdionnegmailcom)\n\n` +
                     `👉 [License](https://github.com/${response.username}/${response.repoName}#license-badges)\n\n` +
                     `👉 [Description](https://github.com/${response.username}/${response.repoName}#description)\n\n` +
@@ -82,41 +84,42 @@ inquirer.prompt([{
                     `👉 [Contributors](https://github.com/${response.username}/${response.repoName}#contributors)\n\n` +
                     `👉 [DNB Solution](https://github.com/${response.username}/${response.repoName}#dnbs-solution)\n\n`),
                 (err) => err ? console.error(err) : console.log(''))
-            }
-// now appending more information by calling the about function
+        }
+        // now appending more information by calling the about function
         about();
+
         function about() {
             fs.appendFile('README.md', (
-// Github username & Email address
-            `\n #### For any questions, reach me via my ![Github Logo](./assets/images/octocat.png?raw=true "Github Logo")username at: [${response.username}](https://github.com/${response.username}) or email me at: 📧${response.email}.\n\n` +
-// license badges
-            `## License:\n This repository is released under the license of: [${response.license}](https://opensource.org/licenses/${response.license})\n\n` +
-// few sentences like in an about / description
-            `## Description: \n${response.about}\n\n` +
-// Installation Instructions: \n
-            `## Installation Instructions: \n${response.instructions}\n\n` +
-//Contributor Section: \n
-            `## Contributor(s): \n${response.contributors}\n\n` +
-// DNB's Solutions with links to repo and github page that are dynamically generated using the repoName and username input values
-            `## DNB's SOLUTION:\n` +
-            `#### UNC Boot Camp Submission: \n` +
-            `🗂️ [Github Repository](https://github.com/${response.username}/${response.repoName}) | 📄 [Github Page](https://${response.username}.github.io/${response.repoName})\n\n`+
-// User Story
-            `## User Story (Usage):\n ${response.userStory}
-            [09-Professional README_Node.js_Demonstration](https://drive.google.com/file/d/1i2ydpq66Lm8Iq8cuaFbdUhQKCRBB1cSr/view?usp=sharing)\n\n` + 
-//--> If user story/acceptable criteria was not provided as a part of input it would be formatted using this instead
-// `## User Story (Usage): \n AS A developer I WANT a README generator SO THAT I can quickly create a professional README for a new project\n\n` +
-// Acceptance Criteria
-            `## Acceptance Criteria:\n ${response.acceptanceCriteria}\n ![Gif Demo](./assets/images/09-Professional_README_Node.js.gif)\n\n`), 
-// alternative hardcoded option: 
-//         `## Acceptance Criteria: \n##### GIVEN a command-line application that accepts user input \n
-// ✓ WHEN I am prompted for information about my application repository THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions\n
-// ✓ WHEN I enter my project title THEN this is displayed as the title of the README\n
-// ✓ WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests\n
-// ✓ WHEN I choose a license for my application from a list of options THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under\n
-// ✓ WHEN I enter my GitHub username THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile\n
-// ✓ WHEN I enter my email address THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions\n
-// ✓ WHEN I click on the links in the Table of Contents THEN I am taken to the corresponding section of the README\n\n`
-            (err) => err ? console.error(err) : console.log(''))
-}
-})
+                    // Github username & Email address
+                    `\n #### For any questions, reach me via my ![Github Logo](./assets/images/octocat.png?raw=true "Github Logo")username at: [${response.username}](https://github.com/${response.username}) or email me at: 📧${response.email}.\n\n` +
+                    // license badges
+                    `## License:\n This repository is released under the license of: [${response.license}](https://opensource.org/licenses/${response.license})\n\n` +
+                    // few sentences like in an about / description
+                    `## Description: \n${response.about}\n\n` +
+                    // Installation Instructions: \n
+                    `## Installation Instructions: \n${response.instructions}\n\n` +
+                    //Contributor Section: \n
+                    `## Contributor(s): \n${response.contributors}\n\n` +
+                    // DNB's Solutions with links to repo and github page that are dynamically generated using the repoName and username input values
+                    `## DNB's SOLUTION:\n` +
+                    `#### UNC Boot Camp Submission: \n` +
+                    `🗂️ [Github Repository](https://github.com/${response.username}/${response.repoName}) | 📄 [Github Page](https://${response.username}.github.io/${response.repoName})\n\n` +
+                    // User Story
+                    `## User Story (Usage):\n ${response.userStory}
+            \n [Video Link - Demonstration](https://drive.google.com/file/d/1i2ydpq66Lm8Iq8cuaFbdUhQKCRBB1cSr/view?usp=sharing)\n\n` +
+                    //--> If user story/acceptable criteria was not provided as a part of input it would be formatted using this instead
+                    // `## User Story (Usage): \n AS A developer I WANT a README generator SO THAT I can quickly create a professional README for a new project\n\n` +
+                    // Acceptance Criteria
+                    `## Acceptance Criteria:\n ${response.acceptanceCriteria}\n ![Gif Demo](./assets/images/09-Professional_README_Node.js.gif)\n\n`),
+                // alternative hardcoded option: 
+                //         `## Acceptance Criteria: \n##### GIVEN a command-line application that accepts user input \n
+                // ✓ WHEN I am prompted for information about my application repository THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions\n
+                // ✓ WHEN I enter my project title THEN this is displayed as the title of the README\n
+                // ✓ WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests\n
+                // ✓ WHEN I choose a license for my application from a list of options THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under\n
+                // ✓ WHEN I enter my GitHub username THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile\n
+                // ✓ WHEN I enter my email address THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions\n
+                // ✓ WHEN I click on the links in the Table of Contents THEN I am taken to the corresponding section of the README\n\n`
+                (err) => err ? console.error(err) : console.log(''))
+        }
+    })
